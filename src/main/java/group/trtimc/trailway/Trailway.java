283179487107d63e94f4fd44f93e196da07956b4
xiaotang27 <zqtang10@qq.com>
2024-08-10 06:46:32 +0800

    Add a new block

--- src/main/java/group/trtimc/trailway/Trailway.java
+++ src/main/java/group/trtimc/trailway/Trailway.java
@@ -3,7 +3,7 @@ package group.trtimc.trailway;
 // import mtr.data.Station;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+// import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.item.*;
